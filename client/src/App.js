c22db35d32bb1bc0710090de08faae88bf660b6b
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-10-21 19:16:30 -0300

    Fetch users from DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import "./App.css"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>App</h1>
+        <h1>Lista de usuarios</h1>
+        {users.length > 0 ? (
+          users.map((user) => (
+            <div key={user.id}>
+              <h1>{user.email}</h1>
+              <h1>{user.name}</h1>
+              <h1>{user.dni}</h1>
+            </div>
+          ))
+        ) : (
+          <h1>No hay usuarios</h1>
+        )}
       </header>
     </div>
   )
