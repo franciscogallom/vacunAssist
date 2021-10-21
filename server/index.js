1521532c1e0c9ce9d0c7ab8fd1d2c52f16a5638b
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-10-21 18:11:09 -0300

    Set up backend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log('hola');
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello world")
+})
+
+app.listen(3001, () => {
+  console.log("servidor corriendo")
+})
