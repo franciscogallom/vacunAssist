ef344af0ad47bb01c6d414537bab4876270590d3
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-10-29 12:28:23 -0300

    First approach to sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,40 @@
 import "./App.css"
-import { useEffect, useState } from "react"
+import homeBannerSVG from "./assets/images/home-banner.svg"
+import Button from "./components/Button/Button"
 
 function App() {
-  const [users, setUsers] = useState([])
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api/users")
-      .then((response) => response.json())
-      .then((data) => setUsers(data))
-      .catch((err) => console.log(err))
-  }, [])
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Lista de usuarios</h1>
-        {users.length > 0 ? (
-          users.map((user) => (
-            <div key={user.id}>
-              <h1>{user.email}</h1>
-              <h1>{user.name}</h1>
-              <h1>{user.dni}</h1>
-            </div>
-          ))
-        ) : (
-          <h1>No hay usuarios</h1>
-        )}
-      </header>
+      <div className="form-container">
+        <form className="form" action="">
+          <input placeholder="Nombre" type="text" />
+          <input placeholder="Apellido" type="text" />
+          <input placeholder="Email" type="text" />
+          <input placeholder="DNI" type="text" />
+          <input placeholder="Contraseña" type="password" />
+          <input placeholder="Repetir contraseña" type="password" />
+
+          <p className="form-label">Fecha de nacimiento.</p>
+          <input className="form-date" type="date" />
+
+          <p className="form-label">Vacunatorio.</p>
+          <select className="select" name="" id="">
+            <option value="vac1">H.S.E. "Elina de la Serna de Montes"</option>
+            <option value="vac2">Estadio "Único" de La Plata</option>
+            <option value="vac3">Polideportivo Los Hornos </option>
+          </select>
+
+          <div className="buttons-container">
+            <Button handleClick={() => alert("Se registra el usuario")} text="Registrarme" />
+            <Button
+              handleClick={() => alert("Navega a /login")}
+              text="Ya tengo cuenta!"
+              secondary
+            />
+          </div>
+        </form>
+      </div>
+      <img className="home-banner" src={homeBannerSVG} alt="Ilustración de médicos" />
     </div>
   )
 }
