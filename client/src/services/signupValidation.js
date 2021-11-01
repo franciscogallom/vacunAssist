db99679717c6ed1d3db8784be5297972811f71f8
Francisco Perez <pacofr2000@gmail.com>
2021-11-01 13:36:10 -0300

    Revert "improving sintax"
    This reverts commit 59c75bebe29613c6b4d2274a7d0aea9153b8eefe.

--- client/src/services/signupValidation.js
+++ client/src/services/signupValidation.js
@@ -1,3 +1,5 @@
+import { version } from "react"
+
 export const signupValidation = ({
     email,
     name,
@@ -5,21 +7,30 @@ export const signupValidation = ({
     dni,
     password,
     verifyPassword,
-    }) => {
+    dateOfBirth
+  }) => {
     const errors = []
     const regexEmail = new RegExp(".+@.+..+", "")
     const regexPassword = new RegExp("^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*])(?=.{8,})")
-    
-    (name.length === 0) && errors.push('Ingrese un nombre.')
-        
-    (lastname.length === 0) && errors.push('Ingrese un apellido.')
-
-    (!regexEmail.test(email)) && errors.push('El email es inválido.')
-
-    (dni.length > 8 || dni.length < 6) && errors.push('El DNI debe contener entre 6 y 8 números.')
-     
-    (!regexPassword.test(password)) && errors.push('La contraseña debe contener 8 o más caracteres, al menos una mayúscula, una minúscula, un número y un símbolo no alfanumérico.')
-    !(verifyPassword === password) && errors.push('Las contraseñas deben coincidir.')
+    const isValidDate = dateOfBirth
+    if(name.length === 0){
+        errors.push('Ingrese un nombre')
+    }
+    if(lastname.length === 0){
+        errors.push('Ingrese un apellido')
+    }
+    if(!regexEmail.test(email)){
+        errors.push('El email es inválido')
+    }
+    if(dni.length > 8 || dni.length < 6){
+        errors.push('El DNI debe contener entre 6 y 8 números')
+    }
+    if(!regexPassword.test(password)){
+        errors.push('La contraseña debe contener 8 o más caracteres, al menos una mayúscula, una minúscula, un número y un símbolo no alfanumérico')
+    }
+    if(!(verifyPassword === password)){
+        errors.push('Las contraseñas deben coincidir')
+    }
 
     return errors
 }
