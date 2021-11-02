220c0b99fac5c2d746ba6bf64aaf658dc68d032a
Francisco Perez <pacofr2000@gmail.com>
2021-11-02 13:20:04 -0300

    back funcionalities

--- server/src/api/routes/auth.js
+++ server/src/api/routes/auth.js
@@ -5,17 +5,54 @@ router.post("/signup", (req, res) => {
   const { email, name, lastname, dni, password, vaccination, date_of_birth } = req.body
   const confirmed = false
   const createdAt = new Date().toLocaleString()
+
   db.query(
-    "INSERT INTO users (email, name, lastname, dni, password, vaccination, date_of_birth, confirmed, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-    [email, name, lastname, dni, password, vaccination, date_of_birth, confirmed, createdAt],
-    (err) => {
-      if (err) {
-        res.send({ err })
+    `SELECT email FROM users WHERE email=${email}`, (error, result) => {
+      if (error) {
+        res.send({ error })
       } else {
-        res.send("Usuario añadido.")
+        if ( result.lenght > 0 ) {
+          res.send("Email ya está en uso.")
+        } else {
+          db.query( 
+            `SELECT dni FROM users WHERE dni=${dni}`, (error, result) => {
+              if (error) {
+                res.send({ error })
+              } else if (result.lenght > 0){
+                  res.send("DNI ya está uso.") 
+              } else {
+                db.query(
+                  "INSERT INTO users (email, name, lastname, dni, password, vaccination, date_of_birth, confirmed, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                  [email, name, lastname, dni, password, vaccination, date_of_birth, confirmed, createdAt],
+                  (err) => {
+                    if (err) {
+                      res.send({ err })
+                    } else {
+                      res.send("Usuario añadido.")
+                    }
+                  }
+                ) 
+              }
+            }
+          )
+        }
       }
     }
   )
+
+})
+
+router.post("/signup/comorbidities", (req, res) => {
+  const { dni, com1, com2, com3, com4, com5, com6, com7, com8, com9, com10, com11, com12 } = req.body
+  db.query("UPDATE users SET com1 = ?,  com2 = ?, com3 = ?, com4 = ?, com5 = ?, com6 = ?, com7 = ?, com8 = ?, com9 = ?, com10 = ?, com11 = ?, com12 = ? WHERE dni = ? ", 
+  [com1, com2, com3, com4, com5, com6, com7, com8, com9 , com10, com11, com12, dni], 
+  (err) => {
+    if (err) {
+      res.send({ err })
+    }else{
+      res.send("Comorbilidades añadidas.")
+    }
+  })
 })
 
 router.post("/login", (req, res) => {
@@ -32,5 +69,8 @@ router.post("/login", (req, res) => {
     }
   })
 })
+
+
+
 
 module.exports = router
