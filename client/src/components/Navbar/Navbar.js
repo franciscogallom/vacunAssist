68e1ed5cc07f0f6e7d29e349232987e5cbe10fd9
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-11-04 19:44:06 -0300

    Add home to Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,12 +2,20 @@ import { Link } from "react-router-dom"
 import vaccine from "../../assets/images/vaccine.png"
 import profile from "../../assets/images/user.png"
 import close from "../../assets/images/close.png"
+import home from "../../assets/images/home.png"
 import "./navbar.css"
 
 function Navbar() {
   return (
     <nav class="navbar">
       <ul class="navbar-nav">
+        <li class="nav-item">
+          <Link to="/home" class="nav-link">
+            <img src={home} alt="Icono de Home" className="img-icon" />
+            <span class="link-text">Home</span>
+          </Link>
+        </li>
+
         <li class="nav-item">
           <Link to="/profile" class="nav-link">
             <img src={profile} alt="Icono de perfil" className="img-icon" />
