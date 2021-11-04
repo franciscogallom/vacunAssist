91809409a152e21d19bf880a41dc4c7705d890b3
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-11-04 16:00:55 -0300

    Vaccines view

--- client/src/components/Vaccines/Vaccines.js
+++ client/src/components/Vaccines/Vaccines.js
@@ -1,10 +1,29 @@
+import "./vaccines.css"
 import Navbar from "../Navbar/Navbar"
+import Form from "../Form/Form"
+import Button from "../Button/Button"
 
 function Vaccines() {
   return (
     <>
       <Navbar />
-      <h1 style={{ marginLeft: "7.5vw" }}>Vacunas</h1>
+      <Form>
+        <h5>Inscripción a vacunas.</h5>
+        <div className="vaccines-container">
+          <div className="vaccine">
+            <p>Coronavirus</p>
+            <Button handleClick={() => alert("click")} text="Inscribirme" />
+          </div>
+          <div className="vaccine">
+            <p>Fiebre amarilla</p>
+            <Button handleClick={() => alert("click")} text="Inscribirme" />
+          </div>
+          <div className="vaccine">
+            <p>Gripe</p>
+            <Button handleClick={() => alert("click")} text="Inscribirme" />
+          </div>
+        </div>
+      </Form>
     </>
   )
 }
