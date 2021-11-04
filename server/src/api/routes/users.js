1ff37b7c266dad378f136b803229c603b75dc0be
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-11-04 17:28:24 -0300

    Add change and check password endpoints

--- server/src/api/routes/users.js
+++ server/src/api/routes/users.js
@@ -7,22 +7,47 @@ router.get("/", (req, res) => {
   })
 })
 
-
 router.get("/:dni", (req, res) => {
   const { dni } = req.params
   db.query(`SELECT name FROM users WHERE dni = ${dni}`, (error, result) => {
     if (error) {
       res.send(error)
     } else {
-        if (result.length === 0){
-          console.log("DNI no existente")
-        } else {
-          res.send( result[0] )
-        }
+      if (result.length === 0) {
+        console.log("DNI no existente")
+      } else {
+        res.send(result[0])
+      }
     }
   })
+})
 
+router.post("/change-password/:dni", (req, res) => {
+  const { password } = req.body
+  const { dni } = req.params
+  db.query("UPDATE users SET password = ? WHERE dni = ? ", [password, dni], (err) => {
+    if (err) {
+      res.send({ err })
+    } else {
+      res.send("Contraseña actualizada.")
+    }
+  })
+})
 
+router.post("/password/:dni", (req, res) => {
+  const { password } = req.body
+  const { dni } = req.params
+  db.query(`SELECT password FROM users WHERE dni = ${dni}`, (err, result) => {
+    if (err) {
+      res.send({ err })
+    } else {
+      if (result[0].password === password) {
+        res.send({ correctPassword: true })
+      } else {
+        res.send({ correctPassword: false })
+      }
+    }
+  })
 })
 
 module.exports = router
