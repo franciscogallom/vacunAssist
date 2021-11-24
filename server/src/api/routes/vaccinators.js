a95a2c6c95403b240117fcfb86cef2ad56fd1026
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-11-24 12:33:45 -0300

    Add user as vaccinator and register in a vaccine

--- server/src/api/routes/vaccinators.js
+++ server/src/api/routes/vaccinators.js
@@ -41,4 +41,62 @@ router.get("/name/:dni", (req, res) => {
   })
 })
 
+router.post("/add-user", (req, res) => {
+  const { email, name, lastname, dni, password, vaccination, date_of_birth, vaccine } = req.body
+  const confirmed = false
+  const createdAt = new Date().toLocaleString()
+
+  db.query(`SELECT email FROM users WHERE email='${email}'`, (error, result) => {
+    if (error) {
+      res.send({ error })
+    } else {
+      if (result[0]) {
+        res.send({ error: true, message: "El email ya está en uso." })
+      } else {
+        db.query(`SELECT dni FROM users WHERE dni=${dni}`, (error, result) => {
+          if (error) {
+            res.send({ error })
+          } else if (result[0]) {
+            res.send({ error: true, message: "El DNI ya está en uso." })
+          } else {
+            db.query(
+              "INSERT INTO users (email, name, lastname, dni, password, vaccination, date_of_birth, confirmed, createdAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+              [
+                email,
+                name,
+                lastname,
+                dni,
+                password,
+                vaccination,
+                date_of_birth,
+                confirmed,
+                createdAt,
+              ],
+              (err) => {
+                if (err) {
+                  res.send({ err })
+                } else {
+                  // El usuario se creo correctamente. Se crea una nueva fila en la tabla "inscriptions"
+                  const date = new Date().toLocaleDateString()
+                  db.query(
+                    `INSERT INTO inscriptions (dni, ${vaccine}) VALUES (?, ?)`,
+                    [dni, date],
+                    (err) => {
+                      if (err) {
+                        res.send(err)
+                      } else {
+                        res.send("Usuario añadido.")
+                      }
+                    }
+                  )
+                }
+              }
+            )
+          }
+        })
+      }
+    }
+  })
+})
+
 module.exports = router
