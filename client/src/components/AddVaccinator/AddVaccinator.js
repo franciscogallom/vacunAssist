935ddd93c5374e8119dfdbaf6063701a209f99a7
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-11-25 19:51:12 -0300

    Validations when admin add vaccinator

--- client/src/components/AddVaccinator/AddVaccinator.js
+++ client/src/components/AddVaccinator/AddVaccinator.js
@@ -3,6 +3,7 @@ import Form from "../Form/Form"
 import Button from "../Button/Button"
 import { addVaccinator } from "../../services/axios/admin"
 import { useState } from "react"
+import { addVaccinatorValidation } from "../../services/addVaccinatorValidation"
 
 function AddVaccinator() {
   const [dni, setDni] = useState("")
@@ -12,34 +13,38 @@ function AddVaccinator() {
   const [password, setPassword] = useState("")
   const [verifyPassword, setVerifyPassword] = useState("")
   const [message, setMessage] = useState("")
-  const [error, setError] = useState("")
+  const [errors, setErrors] = useState([])
 
   const handleAdd = () => {
-    if (
-      name.length === 0 ||
-      lastname.length === 0 ||
-      email.length === 0 ||
-      password.length === 0 ||
-      verifyPassword.length === 0
-    ) {
-      setError("Complete todos los campos.")
+    const validations = addVaccinatorValidation({
+      name,
+      lastname,
+      email,
+      dni,
+      password,
+      verifyPassword,
+    })
+    if (validations.length > 0) {
+      setErrors(validations)
     } else {
-      if (password !== verifyPassword) {
-        setError("Las contraseñas no coinciden.")
-      } else {
-        addVaccinator(email, name, lastname, dni, password)
-          .then((res) => {
-            if (res.data.error) {
-              setError(res.data.message)
-            } else {
-              setMessage(res.data)
-              setError("")
-            }
-          })
-          .catch(() => {
-            setError("Algo salio mal, vuelve a intentarlo.")
-          })
-      }
+      addVaccinator(email, name, lastname, dni, password)
+        .then((res) => {
+          if (res.data.error) {
+            setErrors([res.data.message])
+          } else {
+            setMessage(res.data)
+            setErrors([])
+            document.getElementById("name").value = ""
+            document.getElementById("lastname").value = ""
+            document.getElementById("email").value = ""
+            document.getElementById("dni").value = ""
+            document.getElementById("password").value = ""
+            document.getElementById("verify-password").value = ""
+          }
+        })
+        .catch(() => {
+          setErrors(["Algo salio mal, vuelve a intentarlo."])
+        })
     }
   }
 
@@ -48,24 +53,43 @@ function AddVaccinator() {
       <Navbar />
       <Form>
         <h5>Ingrese los datos del vacunador.</h5>
-        <input onChange={(e) => setName(e.target.value)} placeholder="Nombre" type="text" />
-        <input onChange={(e) => setLastname(e.target.value)} placeholder="Apellido" type="text" />
-        <input onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="text" />
-        <input onChange={(e) => setDni(e.target.value)} placeholder="DNI" type="number" />
         <input
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nombre"
+          type="text"
+        />
+        <input
+          id="lastname"
+          onChange={(e) => setLastname(e.target.value)}
+          placeholder="Apellido"
+          type="text"
+        />
+        <input
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          type="text"
+        />
+        <input id="dni" onChange={(e) => setDni(e.target.value)} placeholder="DNI" type="number" />
+        <input
+          id="password"
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Contraseña"
           type="password"
         />
         <input
+          id="verify-password"
           placeholder="Repetir contraseña"
           type="password"
           onChange={(e) => setVerifyPassword(e.target.value)}
         />
 
-        {error.length > 0 && (
+        {errors.length > 0 && (
           <ul className="form-errors">
-            <li>{error}</li>
+            {errors.map((error, index) => (
+              <li key={index}>{error}</li>
+            ))}
           </ul>
         )}
 
