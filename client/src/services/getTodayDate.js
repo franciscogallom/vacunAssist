e04f6d40589caf2236ed2c7b66f5cfde41be64cd
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-12-08 18:45:37 -0300

    Date improvements

--- client/src/services/getTodayDate.js
+++
@@ -1,6 +0,0 @@
-export const getTodayDate = () => {
-  const todayDate = new Date()
-  const UTCDay = todayDate.getDate()
-  const day = UTCDay < 10 ? `0${UTCDay}` : UTCDay
-  return `Turno para el ${day}/${todayDate.getMonth() + 1}/${todayDate.getFullYear()}.`
-}
