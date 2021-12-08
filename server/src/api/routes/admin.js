4ff86710703125152c3b7cc2f2102d0283fa72e7
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-12-08 19:29:17 -0300

    Improvement on vaccine status update

--- server/src/api/routes/admin.js
+++ server/src/api/routes/admin.js
@@ -77,81 +77,159 @@ router.put("/add-stock/:vaccination", (req, res) => {
 })
 
 router.post("/update-turns", (req, res) => {
-  db.query(`SELECT * FROM inscriptions`, (error, result) => {
+  const today = new Date()
+  today.setDate(today.getDate() - 1) // Para que luego no se actualice cuando el día es el mismo.
+
+  // Actualizo covid.
+  db.query(`SELECT covid, dni FROM inscriptions WHERE covid LIKE '%Turno%'`, (error, result) => {
     if (error) {
       res.send(error)
     } else {
-      const today = new Date()
-      today.setDate(today.getDate() - 1) // Para que luego no se actualice cuando el día es el mismo.
-
       result.map((inscription) => {
-        let covidDate = inscription.covid.slice(14, 24)
-        let feverDate = inscription.fever.slice(14, 24)
-        let fluDate = inscription.flu.slice(14, 24)
+        let date = inscription.covid.split(" ")
+        const arrayDate = date[3].split("/")
+        date = new Date(arrayDate[2], arrayDate[1] - 1, arrayDate[0])
 
-        const arrayCovidDate = covidDate.split("/")
-        const arrayFeverDate = feverDate.split("/")
-        const arrayFluDate = fluDate.split("/")
-
-        covidDate = new Date(arrayCovidDate[2], arrayCovidDate[1] - 1, arrayCovidDate[0])
-        feverDate = new Date(arrayFeverDate[2], arrayFeverDate[1] - 1, arrayFeverDate[0])
-        fluDate = new Date(arrayFluDate[2], arrayFluDate[1] - 1, arrayFluDate[0])
-
-        if (covidDate < today) {
+        if (date < today) {
           db.query(
             `UPDATE inscriptions SET covid = 'No se presento.' WHERE dni = ${inscription.dni}`,
             (error, result) => {
               if (error) {
-                res.send(error)
+                console.log(error)
               } else {
                 console.log(
                   `El usuario ${
                     inscription.dni
-                  } no se presento a su turno para el COVID del dia ${covidDate.toLocaleDateString()}.`
-                )
-              }
-            }
-          )
-        }
-
-        if (feverDate < today) {
-          db.query(
-            `UPDATE inscriptions SET fever = 'No se presento.' WHERE dni = ${inscription.dni}`,
-            (error, result) => {
-              if (error) {
-                res.send(error)
-              } else {
-                console.log(
-                  `El usuario ${
-                    inscription.dni
-                  } no se presento a su turno para la fiebre amarilla del dia ${feverDate.toLocaleDateString()}.`
-                )
-              }
-            }
-          )
-        }
-
-        if (fluDate < today) {
-          db.query(
-            `UPDATE inscriptions SET flu = 'No se presento.' WHERE dni = ${inscription.dni}`,
-            (error, result) => {
-              if (error) {
-                res.send(error)
-              } else {
-                console.log(
-                  `El usuario ${
-                    inscription.dni
-                  } no se presento a su turno para la gripe del dia ${fluDate.toLocaleDateString()}.`
+                  } no se presento a su turno para el COVID del dia ${date.toLocaleDateString()}.`
                 )
               }
             }
           )
         }
       })
-
-      res.send("Actualizaciones realizadas.")
     }
   })
+
+  // Actualizo covid2.
+  db.query(`SELECT covid2, dni FROM inscriptions WHERE covid2 LIKE '%Turno%'`, (error, result) => {
+    if (error) {
+      res.send(error)
+    } else {
+      result.map((inscription) => {
+        let date = inscription.covid2.split(" ")
+        const arrayDate = date[3].split("/")
+        date = new Date(arrayDate[2], arrayDate[1] - 1, arrayDate[0])
+
+        if (date < today) {
+          db.query(
+            `UPDATE inscriptions SET covid2 = 'No se presento.' WHERE dni = ${inscription.dni}`,
+            (error, result) => {
+              if (error) {
+                console.log(error)
+              } else {
+                console.log(
+                  `El usuario ${
+                    inscription.dni
+                  } no se presento a su turno para el COVID2 del dia ${date.toLocaleDateString()}.`
+                )
+              }
+            }
+          )
+        }
+      })
+    }
+  })
+
+  // Actualizo fever.
+  db.query(`SELECT fever, dni FROM inscriptions WHERE fever LIKE '%Turno%'`, (error, result) => {
+    if (error) {
+      res.send(error)
+    } else {
+      result.map((inscription) => {
+        let date = inscription.fever.split(" ")
+        const arrayDate = date[3].split("/")
+        date = new Date(arrayDate[2], arrayDate[1] - 1, arrayDate[0])
+
+        if (date < today) {
+          db.query(
+            `UPDATE inscriptions SET fever = 'No se presento.' WHERE dni = ${inscription.dni}`,
+            (error, result) => {
+              if (error) {
+                console.log(error)
+              } else {
+                console.log(
+                  `El usuario ${
+                    inscription.dni
+                  } no se presento a su turno para FIEBRE del dia ${date.toLocaleDateString()}.`
+                )
+              }
+            }
+          )
+        }
+      })
+    }
+  })
+
+  // Actualizo gripe.
+  db.query(`SELECT flu, dni FROM inscriptions WHERE flu LIKE '%Turno%'`, (error, result) => {
+    if (error) {
+      res.send(error)
+    } else {
+      result.map((inscription) => {
+        let date = inscription.flu.split(" ")
+        const arrayDate = date[3].split("/")
+        date = new Date(arrayDate[2], arrayDate[1] - 1, arrayDate[0])
+
+        if (date < today) {
+          db.query(
+            `UPDATE inscriptions SET flu = 'No se presento.' WHERE dni = ${inscription.dni}`,
+            (error, result) => {
+              if (error) {
+                console.log(error)
+              } else {
+                console.log(
+                  `El usuario ${
+                    inscription.dni
+                  } no se presento a su turno para la GRIPE del dia ${date.toLocaleDateString()}.`
+                )
+              }
+            }
+          )
+        }
+      })
+    }
+  })
+
+  // Actualizo gripe2.
+  db.query(`SELECT flu2, dni FROM inscriptions WHERE flu2 LIKE '%Turno%'`, (error, result) => {
+    if (error) {
+      res.send(error)
+    } else {
+      result.map((inscription) => {
+        let date = inscription.flu2.split(" ")
+        const arrayDate = date[3].split("/")
+        date = new Date(arrayDate[2], arrayDate[1] - 1, arrayDate[0])
+
+        if (date < today) {
+          db.query(
+            `UPDATE inscriptions SET flu2 = 'No se presento.' WHERE dni = ${inscription.dni}`,
+            (error, result) => {
+              if (error) {
+                console.log(error)
+              } else {
+                console.log(
+                  `El usuario ${
+                    inscription.dni
+                  } no se presento a su turno para la GRIPE2 del dia ${date.toLocaleDateString()}.`
+                )
+              }
+            }
+          )
+        }
+      })
+    }
+  })
+  res.send("Actualizaciones realizadas.")
 })
 
 module.exports = router
